9d79bcae4b37d806a599e5d0fcb5b90851819c4a
Jeff Scott <jeffscott2@gmail.com>
2018-05-14 21:40:34 -0400

    Make data frame creation simpler

--- 3.pandas.py
+++ 3.pandas.py
@@ -5,29 +5,35 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-row_numbers = range(0,60)
-ages = [row + 15 for row in row_numbers]
-age_data = list(zip(row_numbers, ages))
-
-df = pd.DataFrame(data = age_data, columns=['Row', 'Age'])
-df.set_index('Row')
-
 # Important:
+age_you_start_saving_at = 15
+life_expectancy = 80
 rate_of_return = .04
 yearly_savings = 1000
 
 
+ages = list(range(1, life_expectancy + 1))
+df = pd.DataFrame(data = ages, columns=['Age'])
+
+
 prev_total = 0
-for row in row_numbers:
+for i in range(0, len(ages)):
+
+    age = df.loc[i,'Age']
+
+    if age < age_you_start_saving_at:
+        actual_yearly_savings = 0
+    else:
+        actual_yearly_savings = yearly_savings
 
     year_start_money = prev_total
-    investment_returns = round( (year_start_money+yearly_savings) * rate_of_return,0)
-    year_ending_money = year_start_money + yearly_savings + investment_returns
+    investment_returns = round( year_start_money * rate_of_return,0)
+    year_ending_money = year_start_money + actual_yearly_savings + investment_returns
 
-    df.loc[row,'YearStartingMoney'] = year_start_money
-    df.loc[row,'YearlySaving'] = yearly_savings
-    df.loc[row,'InvestmentReturn'] = investment_returns
-    df.loc[row,'YearEndingMoney'] = year_ending_money
+    df.loc[i,'YearStartingMoney'] = year_start_money
+    df.loc[i,'InvestmentReturn'] = investment_returns
+    df.loc[i,'YearlySaving'] = actual_yearly_savings
+    df.loc[i,'YearEndingMoney'] = year_ending_money
 
     prev_total = year_ending_money
 
